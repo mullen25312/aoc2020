04bb44e0e6aaf3f3441cee8705f73ee646412c01
mullen25312 <mail@benjaminjahn.de>
2020-12-06 17:48:27 +0100

    refactor: Simplify solution of day06p1 and day06p2

--- d06/d06.py
+++ d06/d06.py
@@ -4,36 +4,22 @@ class DailyPuzzle06:
 
     def read_data(self):
         with open("./d06/input.txt") as f:
-            self.data = f.readlines()
+
+            groups = []
+            group = []
+            for line in f:
+                if line == "\n":
+                    groups.append(group)
+                    group = []
+                else:
+                    passenger = set(list(line[:-1]))
+                    group.append(passenger)
+
+            groups.append(group)
+        self.data = groups
 
     def solve_part_one(self):
-        groups = []
-        group = set()
-        for line in self.data:
-            if line == "\n":
-                groups.append(group)
-                group = set()
-            else:
-                passenger = set(list(line[:-1]))
-                group = group.union(passenger)
-
-        groups.append(group)
-
-        number_of_answers = map(len, groups)
-        return sum(number_of_answers)
+        return sum(map(len, map(lambda group: set.union(*group), self.data)))
 
     def solve_part_two(self):
-        groups = []
-        group = set(list(self.data[0][:-1]))
-        for idx, line in enumerate(self.data):
-            if line == "\n":
-                groups.append(group)
-                group = set(list(self.data[idx + 1][:-1]))
-            else:
-                passenger = set(list(line[:-1]))
-                group = group.intersection(passenger)
-
-        groups.append(group)
-
-        number_of_answers = map(len, groups)
-        return sum(number_of_answers)
+        return sum(map(len, map(lambda group: set.intersection(*group), self.data)))
